c9247dbad4f143f11d5d44d29d922e2bf05f33b4
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2015-12-28 01:25:55 +0000

    took out unnecessary line

--- q5_1/src/HashUtilities.java
+++ q5_1/src/HashUtilities.java
@@ -4,7 +4,6 @@
 public class HashUtilities {
 
     public static int shortHash(int number) {
-        int hash = Math.abs(number%1000);
-        return hash;
+        return Math.abs(number%1000);
     }
 }
